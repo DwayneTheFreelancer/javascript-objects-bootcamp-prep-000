e56216232d85f71bbe492c28316e506ac281a2b7
Dwayne Munro <dwaynemunro1845@gmail.com>
2018-08-28 04:05:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,9 +2,12 @@ var playlist = {"Dave East": "Rover 2.0 Remix"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, {[artistName] : songTitle});
+<<<<<<< HEAD
 }
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
   return Object.assign(playlist, {["Alison"] : "Slowdive"});
+=======
+>>>>>>> 7ab5c97782148c14f6a7cb37d3134b200b513668
 }
